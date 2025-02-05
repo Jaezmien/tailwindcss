c087c14d0c64eaaac8fb1af823bab5eca157c62e
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2025-02-05 23:55:24 +0800

    fix: Return a string option instead of using withOpacity

--- src/index.ts
+++ src/index.ts
@@ -78,12 +78,11 @@ const generateColorCss = (
 // generates the 'options' mapping in tailwind.config.js
 // this extends the theme & adds the names of the colors
 const generateOptions = (prefix: string | boolean = false) => {
-  const result: Record<string, { DEFAULT: WithOpacityFn }> = {};
+  const result: Record<string, string> = {};
 
   colors.map((color) => {
     const keyName = prefix ? `${prefix}-${color}` : color;
-    // withOpacity is used to provide backward compatibility with Tailwind < 3.1
-    result[keyName] = { DEFAULT: withOpacity(`--ctp-${color}`) };
+    result[keyName] = `rgb(var(--ctp-${color}))`;
   });
 
   return result;
